37f9eca582722ab8c5ffd47bffaa671b429dcca5
CustoomNPC <CustoomNPC@gmail.com>
2022-03-13 12:25:15 +0900

    interaction.userId x interaction.user.id o

--- commands/track.js
+++ commands/track.js
@@ -35,7 +35,7 @@ module.exports = {
 	 */
 	async execute(interaction, client) {
 		try {
-			if (using.includes(interaction.userId)) return interaction.reply({ embeds: [new MessageEmbed().setTitle("❌ 기다리쇼").setColor("RED")] });
+			if (using.includes(interaction.user.id)) return interaction.reply({ embeds: [new MessageEmbed().setTitle("❌ 기다리쇼").setColor("RED")] });
 			// if (!config.owners.includes(interaction.user.id) && !config.allowedUsers.includes(interaction.user.id)) return interaction.reply({ embeds: [new MessageEmbed().setTitle("❌ Missing Permission").setDescription("You don't have permission to use this command.").setColor("RED")], ephemeral: true });
 			let startedTime = Date.now();
 			let region = interaction.options.getString("지역");
